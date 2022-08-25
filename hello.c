6e5da7acd3b82ef0ea653697865cbf99eff290bb
NickWayman <nwayman2@huskers.unl.edu>
2022-08-25 21:18:14 +0000

    Initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Nicholas Wayman
+ *         nwayman2@huskers.unl.edu
+ * Date: 2022/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("My name is Nicholas Wayman\n");
+  printf("\n");
+  printf("The School of Computing Rocks! \n");
+  return 0;
+}
