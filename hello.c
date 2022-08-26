b5906e9807d4933e43c48230dd468db9c728ceb1
AndrewDuw <aduweling2@huskers.unl.edu>
2022-08-26 22:11:22 +0000

    added my major too

--- hello.c
+++ hello.c
@@ -1,18 +1,15 @@
 /**
- * Author: Nicholas Wayman
- *         nwayman2@huskers.unl.edu
- * Date: 2022/08/23
- *
- * A simple hello world program in C
- *
+ * Basic Hello World Program
  */
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char **argv) {
 
-  printf("My name is Nicholas Wayman\n");
-  printf("I plan to major in Chemical Engineering\n");
-  printf("I plan to minor in Computer Science\n");
-  return 0;
+    printf("Hello, I'm Nicholas Wayman!\n");
+    printf("Hello, I'm Andrew Duweling!\n");
+    printf("Our Majors are Chemical Engineering and Computer Engineering!\n");
+    printf("Minor is Computer Science\n");
+
+    return 0;
 }
